05bf851b75b1652066538da5ce6834c14c3d1248
DawnEggleton <dawn.eggleton@carleton.ca>
2024-01-02 23:38:39 -0400

    profile touchups

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -20,6 +20,50 @@ function capitalizeMultiple(selector) {
     document.querySelectorAll(selector).forEach(character => {
         character.innerText = capitalize(character.innerText);
     });
+}function setMonth(month) {
+    switch(month) {
+        case 'January':
+            month = 1;
+            break;
+        case 'February':
+            month = 2;
+            break;
+        case 'March':
+            month = 3;
+            break;
+        case 'April':
+            month = 4;
+            break;
+        case 'May':
+            month = 5;
+            break;
+        case 'June':
+            month = 6;
+            break;
+        case 'July':
+            month = 7;
+            break;
+        case 'August':
+            month = 8;
+            break;
+        case 'September':
+            month = 9;
+            break;
+        case 'October':
+            month = 10;
+            break;
+        case 'November':
+            month = 11;
+            break;
+        case 'December':
+            month = 12;
+            break;
+        default:
+            month = -1;
+            break;
+    }
+
+    return month;
 }
 
 /****** Settings ******/
